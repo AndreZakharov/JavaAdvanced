4138ec556e5d2a57578f358d0390ce7c2fb93904
AndreZakharov <122970684+AndreZakharov@users.noreply.github.com>
2023-02-28 23:33:09 +0300

    section 1 task 1

---
+++ src/main/java/module1/task01/Calculator.java
@@ -0,0 +1,32 @@
+package module1.task01;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * created on 28.02.22
+ * @author Andre
+ * @see #additionMethot(double, double)
+ */
+
+public class Calculator {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        System.out.println("Enter 2 numbers (double, use a comma)");
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        System.out.printf("%.4f",calculator.additionMethot(a,b));
+
+    }
+
+    /**
+     *
+     * @param a input double first value
+     * @param b input double second value
+     * @return double addition result
+     */
+    public double additionMethot(double a,double b){
+        return a+b;
+    }
+}
